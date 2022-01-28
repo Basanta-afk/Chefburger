dc9a4dd1c124a66bb8b11db5f369afe28e3f4893
Basanta-afk <67224509+Basanta-afk@users.noreply.github.com>
2022-01-28 15:09:17 +0545

    inital commit

--- Chefburger/settings.py
+++ Chefburger/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -126,3 +128,5 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
